096850b7fd727cd9291e24111d731754dd04dbb0
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-25 11:48:20 -0700

    Implement routes in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
