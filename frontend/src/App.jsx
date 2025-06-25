931c201a79ddf6e41d53113b5caad976baff0c91
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-25 11:48:06 -0700

    Add HomePage route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
-    <div>
-      <h1>Finance Companion</h1>
-    </div>
-  );
+    <>
+      <HomePage />
+    </>
+  )
 }
 
 export default App;
