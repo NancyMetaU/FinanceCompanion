8c3cc00e8788e4a789efdf1a9339ee669c685e3d
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-25 11:47:50 -0700

    Create NotFoundPage skeleton

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-page">
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you’re looking for doesn’t exist.</p>
+      <Link to="/" className="home-link">
+        Go to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
