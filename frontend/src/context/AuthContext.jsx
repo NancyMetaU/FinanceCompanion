587974c6b0b962261d520c5d9609be60d76127d7
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-26 14:36:51 -0700

    Reformat authContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,27 +1,25 @@
-import { createContext } from 'react';
+import { createContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState, useContext } from 'react';
+
+// TODO: Add a loading state handling
 
 const auth = getAuth();
 const AuthContext = createContext();
 
-// TODO: Add a loading state handling
-
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-        return () => unsubscribe();
-    }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ user }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
