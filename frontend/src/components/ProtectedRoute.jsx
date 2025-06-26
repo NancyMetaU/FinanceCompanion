311053f0629391f2239f082e1650c5d395068859
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-26 16:35:54 -0700

    Add state components to Route Protector

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
+import Loading from "./Loading";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  // TODO: Create a reusable loading component
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loading message="Checking authentication..." />;
   return user ? children : <Navigate to="/login" />;
 };
 
