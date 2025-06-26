4bb18f3be04b7e96610b827841402a93be0e30cf
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-26 15:54:06 -0700

    Create pictureSlider component

---
+++ frontend/src/home-page-components/PictureSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PictureSlider = () => {
+  return (
+    <div className="picture-slider">
+      <p className="slide active">Image Slide will go here</p>
+    </div>
+  );
+};
+
+export default PictureSlider;
