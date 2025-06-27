5f6908883ef70aea082c941fd8ee9050cbe258b5
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-27 10:31:28 -0700

    Add content container

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,11 +1,14 @@
 import AuthForm from "../components/AuthForm";
 
 const AuthPage = () => {
-    return (
-        <div>
-            <AuthForm />
-        </div>
-    );
+  return (
+    <div className="auth-page">
+      <div className="auth-container">
+        <h1 className="auth-page-title">WELCOME</h1>
+        <AuthForm />
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
