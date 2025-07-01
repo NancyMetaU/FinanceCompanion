c95618e0173279ac12ed510cf840cc68b50192cc
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-01 16:49:34 -0700

    Add route to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const plaidTokenRoutes = require("./routes/plaidTokenRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
+const accountRoutes = require("./routes/accountRoutes");
 
 const server = express();
 server.use(helmet());
@@ -10,6 +11,7 @@ server.use(express.json());
 server.use(cors());
 server.use("/api/plaid", plaidTokenRoutes);
 server.use("/api/transactions", transactionRoutes);
+server.use("/api/accounts", accountRoutes);
 
 server.use("*", (req, res, next) => {
   next({ status: 404, message: "Not found" });
