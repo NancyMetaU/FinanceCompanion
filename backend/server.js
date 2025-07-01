b66bae919b860f4c3738ba6f8f5790a4d346ba8d
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-01 16:05:10 -0700

    Refactor server files

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const plaidTokenRoutes = require("./routes/plaidTokenRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use("/api/plaid", plaidTokenRoutes);
+server.use("/api/transactions", transactionRoutes);
+
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "Not found" });
+});
+
+server.use((err, req, res, next) => {
+  const { message, status } = err;
+  console.error("Error: ", message);
+  res.status(status).json({ message });
+});
+
+module.exports = server;
