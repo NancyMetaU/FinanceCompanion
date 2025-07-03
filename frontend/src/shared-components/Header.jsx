673cd0efac173f4272c3968eb6cf94a548495ecc
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-03 11:24:58 -0700

    Increase logo size

--- frontend/src/shared-components/Header.jsx
+++ frontend/src/shared-components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-slate-100 shadow-md">
       <div className="flex items-center space-x-4">
-        <img src={logo} alt="Logo" className="h-10 w-10" />
+        <img src={logo} alt="Logo" className="h-15 w-15" />
         <h1 className="text-2xl font-bold text-slate-800">Finance Companion</h1>
       </div>
 
