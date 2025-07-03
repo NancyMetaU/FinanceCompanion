c7ffa68a7f315b743c9d67842118e9506fa56832
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-03 11:19:03 -0700

    Created shared components

---
+++ frontend/src/shared-components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto w-full bg-slate-100 text-left text-sm text-slate-500 py-4 px-6">
+      &copy; {new Date().getFullYear()} Finance Companion
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/shared-components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "/src/assets/finance-companion-logo.png";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-6 py-4 bg-slate-100 shadow-md">
+      <div className="flex items-center space-x-4">
+        <img src={logo} alt="Logo" className="h-10 w-10" />
+        <h1 className="text-2xl font-bold text-slate-800">Finance Companion</h1>
+      </div>
+
+      <button
+        className="text-sm font-medium text-[#00009A] border border-[#00009A] px-4 py-1.5
+      rounded-md hover:bg-[#00009A] hover:text-white transition mr-4"
+      >
+        Sign Out
+      </button>
+    </header>
+  );
+};
+
+export default Header;
