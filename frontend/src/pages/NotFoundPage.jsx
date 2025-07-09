c21545a2d028a6e079fdbbab8300dc0c9dd34c92
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-08 22:33:51 -0700

    Add simple style to NotFoundPage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div className="not-found-page">
-      <h1>Page Not Found</h1>
-      <p>Sorry, the page you’re looking for doesn’t exist.</p>
-      <Link to="/" className="home-link">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 px-4">
+      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+      <h2 className="text-2xl font-semibold text-gray-700 mb-2">
+        Page Not Found
+      </h2>
+      <p className="text-gray-600 mb-8">
+        Sorry, the page you're looking for doesn't exist.
+      </p>
+      <Link
+        to="/"
+        className="bg-royal hover:bg-blue-800 text-white px-6 py-3 rounded-lg"
+      >
         Go to Home
       </Link>
     </div>
