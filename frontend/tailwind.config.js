ad5fbdcb2919e9080c4b6e30713f37b7de0b3cd7
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-24 14:37:24 -0700

    Add animations to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,77 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        royal: "#4361ee",
+      },
+      animation: {
+        "fade-in-up": "fadeInUp 0.5s ease-out forwards",
+        "fade-in": "fadeIn 0.5s ease-out forwards",
+        bounce: "bounce 1s infinite",
+        pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-25%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+        pulse: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.8" },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".animation-delay-100": {
+          "animation-delay": "100ms",
+        },
+        ".animation-delay-200": {
+          "animation-delay": "200ms",
+        },
+        ".animation-delay-300": {
+          "animation-delay": "300ms",
+        },
+        ".animation-delay-400": {
+          "animation-delay": "400ms",
+        },
+        ".animation-delay-500": {
+          "animation-delay": "500ms",
+        },
+        ".animation-delay-600": {
+          "animation-delay": "600ms",
+        },
+        ".animation-delay-700": {
+          "animation-delay": "700ms",
+        },
+        ".animation-delay-800": {
+          "animation-delay": "800ms",
+        },
+        ".animation-delay-900": {
+          "animation-delay": "900ms",
+        },
+        ".animation-delay-1000": {
+          "animation-delay": "1000ms",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
