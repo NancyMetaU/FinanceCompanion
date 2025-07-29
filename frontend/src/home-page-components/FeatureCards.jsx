794a2404da52e3baf55f51e1d8c44772ab81d22b
Nancy Cristina Farias <nancyfar@meta.com>
2025-07-29 11:10:16 -0700

    Switch feature cards to require auth

--- frontend/src/home-page-components/FeatureCards.jsx
+++ frontend/src/home-page-components/FeatureCards.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { features } from "./featureList.jsx";
-import FeatureCard from "./FeatureCard";
+import AuthAwareFeatureCard from "./AuthAwareFeatureCard";
 
 const FeatureCards = () => {
   return (
     <ul className="grid grid-cols-1 md:grid-cols-3 gap-10 py-12 w-5/6 mx-auto list-none p-0">
       {features.map((feat) => (
         <li key={feat.title} className="m-0 p-0">
-          <FeatureCard {...feat} />
+          <AuthAwareFeatureCard {...feat} />
         </li>
       ))}
     </ul>
